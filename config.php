3c5b980ed18a88858735f09074a53b8bcd7b3bbd
Sachin <121999384+sachindinuja@users.noreply.github.com>
2024-10-07 16:55:37 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,12 +1,12 @@
 <?php
 // SQLSRV Extension Sample Code (optional, remove if not needed)
 $connectionInfo = array("UID" => "azuresql", "pwd" => "Alpha@0515", "Database" => "abcuni", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-$serverName = "tcp:it21354410abcuni.database.windows.net,1433";
+$serverName = "tcp:it21238376abcuni.database.windows.net,1433";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
 
 // PDO Connection
 try {
-    $pdo = new PDO("sqlsrv:server = tcp:it21354410abcuni.database.windows.net,1433; Database = abcuni", "azuresql", "Alpha@0515");
+    $pdo = new PDO("sqlsrv:server = tcp:it21238376abcuni.database.windows.net,1433; Database = abcuni", "azuresql", "Alpha@0515");
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     print("Error connecting to SQL Server.");
